7306bad7b24361d7ea39a5d03440731895fe36ae
FulgurIgor <i.s.ger@ya.ru>
2018-11-20 15:42:26 -0500

    autocommit

--- R.R
+++ R.R
@@ -22,7 +22,10 @@ csv <- data %>%
 P <- ggplot(csv, aes(x=timestamp, y=votes, color=name)) +
   geom_line() +
   theme_bw() +
-  labs(x = "Время, EST", y = "Число голосов", color = "Имя участника")
+  labs(x = "Время, EST", y = "Число голосов", color = "Имя участника") +
+  scale_y_continuous(breaks = seq(0,50000, 1000)) +
+  scale_x_datetime(labels = function(x) strftime(x, "%d/%m %H:%M"), date_breaks = "2 hour") +
+  theme(axis.text.x=element_text(angle=60, hjust=1))
 
 PP <- ggplotly(P)
 saveWidget(PP, file = '/home/ger/Proj/premia/fulgurigor.github.io/index.html', selfcontained=T, title="Премия Рунета 2018")
